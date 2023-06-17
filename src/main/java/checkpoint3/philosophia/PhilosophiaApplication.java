0b4f37f9f2ac8da96f924594ba84e866c2aaa9fe
mehdy guettache <mehdy33852@gmail.com>
2023-06-17 20:56:15 +0200

    first push

---
+++ src/main/java/checkpoint3/philosophia/PhilosophiaApplication.java
@@ -0,0 +1,13 @@
+package checkpoint3.philosophia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilosophiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilosophiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/checkpoint3/philosophia/PhilosophiaApplicationTests.java
@@ -0,0 +1,13 @@
+package checkpoint3.philosophia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhilosophiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
